4038366cc75d3ce3cc46e59acfa1e8cb6011fcc1
Anastasia Lobanova <lobanovanastasia@mail.ru>
2024-07-01 22:29:44 +0500

    Change pom

---
+++ src/main/java/org/example/MovieManager.java
@@ -0,0 +1,6 @@
+import org.example.PosterMovie;
+
+public class MovieManager {
+    private int limit = 10;
+    private Poster
+}
---
+++ src/main/java/org/example/PosterMovie.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class PosterMovie {
+    private int movieId;
+    private String movieName;
+    private String ganre;
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getGanre() {
+        return ganre;
+    }
+
+    public void setGanre(String ganre) {
+        this.ganre = ganre;
+    }
+
+
+}
